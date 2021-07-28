0f194eee1ea2e0e57364b3f4c0bd849a473cfad3
Georg Pirklbauer <georg.pirklbauer@dynatrace.com>
2021-07-28 15:48:12 +0200

    Add Github Action to publish releases to PyPI (#17)
    Co-authored-by: Armin Ruech <armin.ruech@dynatrace.com>

--- setup.py
+++ setup.py
@@ -7,11 +7,4 @@
 #    http://www.apache.org/licenses/LICENSE-2.0
 import setuptools
 
-setuptools.setup(
-    name="dynatrace-opentelemetry-metrics-export",
-    install_requires=[
-        "opentelemetry-api==0.15b0",
-        "opentelemetry-sdk==0.15b0",
-        "requests~=2.25",
-    ]
-)
+setuptools.setup()
