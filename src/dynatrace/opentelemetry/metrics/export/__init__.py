ef8bea9539c3a643d96971d7c1145f42e825f9fc
Georg Pirklbauer <georg.pirklbauer@dynatrace.com>
2021-07-28 15:51:09 +0200

    add .0 to version

--- src/dynatrace/opentelemetry/metrics/export/__init__.py
+++ src/dynatrace/opentelemetry/metrics/export/__init__.py
@@ -25,7 +25,7 @@ from opentelemetry.sdk.metrics.export import (
 from .serializer import DynatraceMetricsSerializer
 from .oneagentmetadataenricher import OneAgentMetadataEnricher
 
-VERSION = "0.1.0-beta"
+VERSION = "0.1.0-beta.0"
 
 
 class DynatraceMetricsExporter(MetricsExporter):
